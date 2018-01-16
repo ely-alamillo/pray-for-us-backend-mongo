6224444a5a4c41c5c452c85c0c481a686575a654
Ely Alamillo <ely0013@gmail.com>
2018-01-15 22:17:22 -0600

    modified user model

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const { postSchema } = require('./postModel')
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique:true },
     password: { type: String, required: true},
-    likedPosts: [Posts],
+    likedPosts: [postSchema],
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User,
+    userSchema
+}
