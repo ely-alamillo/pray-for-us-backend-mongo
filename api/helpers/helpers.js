930d90d0d62c1427ac5778c3b4755d6e56c02604
Ely Alamillo <ely0013@gmail.com>
2018-01-22 22:48:20 -0600

    added sendUserError helper

---
+++ api/helpers/helpers.js
@@ -0,0 +1,14 @@
+const SERVER_USER_ERROR = 422;
+
+export const sendUserError = (err, res) => {
+  res.status(SERVER_USER_ERROR);
+  if (typeof err === 'string') {
+    return res.json({ err });
+  } else if (err && err.message) {
+    return res.json({
+      message: err.message,
+      stack: err.stack
+    });
+  }
+  res.json(err);
+};
