0500703e438f3ea533da58235211e2d7ad5308ba
Ely Alamillo <ely0013@gmail.com>
2018-01-23 00:27:38 -0600

    moved fiebase auth to userController

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
-const admin = require('firebase-admin');
+// const admin = require('firebase-admin');
+const { routes } = require('./api/routes');
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.PROJECT_ID,
-    clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: `${process.env.PRIVATE_KEY}`,
-  })
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert({
+//     type: process.env.TYPE,
+//     project_id: process.env.PROJECT_ID,
+//     private_key_id: process.env.PRIVATE_KEY_ID,
+//     private_key: `${process.env.PRIVATE_KEY}`,
+//     client_email: process.env.CLIENT_EMAIL,
+//     client_id: process.env.CLIENT_ID,
+//     auth_uri: process.env.AUTH_URI,
+//     token_uri: process.env.TOKEN_URI,
+//     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+//     client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+//   })
+// });
 
 mongoose.Promise = global.Promise;
 
@@ -38,6 +46,7 @@ const connect = mongoose.connect(
 connect
   .then(() => {
     logger.log('Connected to MongoDB...');
+    routes(app);
     app.listen(process.env.PORT, () => {
       logger.log(`Server listening on port ${process.env.PORT}`);
     });
